fe066eb0dc91c2a11bb9d468efb0aba2455183f1
kavish28-kid <kavishagrawal80877@gmail.com>
2026-07-04 23:20:34 +0530

    change base path to / for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/photobooth/",
+  base: "/",
   plugins: [react()],
   build: {
     cssCodeSplit: true,
